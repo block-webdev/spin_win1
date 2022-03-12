a3ba0f042df1a902bd646f3e34537b027e2b9463
block-webdev <akiowebdev@gmail.com>
2022-03-12 19:36:33 +0100

    final

---
+++ programs/spin_win/src/lib.rs
@@ -0,0 +1,190 @@
+use anchor_lang::prelude::*;
+use anchor_lang::solana_program::{clock};
+use anchor_spl::token::{self, CloseAccount, Mint, SetAuthority, TokenAccount, Transfer};
+use spl_token::instruction::AuthorityType;
+
+declare_id!("rnmMFkkuYdz1Gz3V4Pzn1dJb1UasQeYgoAi2WixqiEV");
+
+#[program]
+pub mod spin_win {
+    use super::*;
+
+    pub const ESCROW_PDA_SEED: &str = "sw_game_seeds";
+    pub const SPIN_ITEM_COUNT: usize = 15;
+
+    pub fn initialize(
+        ctx: Context<Initialize>, _pool_bump: u8,
+    ) -> ProgramResult {
+        msg!("initialize");
+
+        let state = &mut ctx.accounts.state;
+        state.amount_list = [0; SPIN_ITEM_COUNT];
+        state.ratio_list = [0; SPIN_ITEM_COUNT];
+
+        state.nonce = _pool_bump;
+
+        Ok(())
+    }
+
+    pub fn set_item(
+        ctx: Context<SetItem>,
+        token_vault_bump: u8,
+        ratio: [u8; 15],
+        amount: [u64; 15],
+    ) -> ProgramResult {
+        msg!("set_item");
+
+        let state = &mut ctx.accounts.state;
+        state.ratio_list = ratio;
+        state.amount_list = amount;
+
+        // let cpi_ctx = CpiContext::new(
+        //     ctx.accounts.token_program.to_account_info().clone(),
+        //     token::Transfer {
+        //         from: ctx.accounts.reward_account.to_account_info(),
+        //         to: ctx.accounts.token_vault.to_account_info(),
+        //         authority: ctx.accounts.owner.to_account_info(),
+        //     },
+        // );
+        // token::transfer(cpi_ctx, amount)?;
+
+        Ok(())
+    }
+
+    pub fn spin_wheel(ctx: Context<SpinWheel>) -> ProgramResult {
+        let state = &mut ctx.accounts.state;
+        let spin_index: u8 = get_spinresult(state) as u8;
+        state.last_spinindex = spin_index;
+
+        return Ok(());
+    }
+
+    pub fn transfer_rewards(ctx: Context<TransferRewards>, spin_index: u8) -> ProgramResult {
+
+        let state = &ctx.accounts.state;
+        let amount = state.amount_list[spin_index as usize];
+
+        let cpi_ctx = CpiContext::new(
+            ctx.accounts.token_program.to_account_info().clone(),
+            token::Transfer {
+                from: ctx.accounts.token_vault.to_account_info(),
+                to: ctx.accounts.dest_account.to_account_info(),
+                authority: ctx.accounts.owner.to_account_info(),
+            },
+        );
+        token::transfer(cpi_ctx, amount)?;
+
+        Ok(())
+    }
+}
+
+fn get_spinresult(state: &mut SpinItemList) -> u8 {
+    let c = clock::Clock::get().unwrap();
+    let r = (c.unix_timestamp % 100) as u8;
+    let mut start = 0;
+    for (pos, item) in state.ratio_list.iter().enumerate() {
+        let end = start + item;
+        if r >= start && r < end {
+            return pos as u8;
+        }
+        start = end;
+    }
+
+    return 0;
+}
+
+#[derive(Accounts)]
+#[instruction(_bump: u8)]
+pub struct Initialize<'info> {
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    #[account(mut, signer)]
+    initializer : AccountInfo<'info>,
+
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    #[account(init, payer=initializer, seeds=[ESCROW_PDA_SEED.as_ref()], bump = _bump)]
+    state : Account<'info, SpinItemList>,
+
+    system_program: Program<'info, System>,
+}
+
+
+#[account]
+#[derive(Default)]
+pub struct SpinItemList {
+    ratio_list: [u8; SPIN_ITEM_COUNT],
+    amount_list: [u64; SPIN_ITEM_COUNT],
+    nonce: u8,
+    last_spinindex: u8,
+}
+
+
+#[derive(Accounts)]
+#[instruction(_token_bump: u8)]
+pub struct SetItem<'info> {
+    /// CHECK: this is not dangerous.
+    #[account(mut, signer)]
+    owner : AccountInfo<'info>, 
+
+    /// CHECK: this is not dangerous.
+    #[account(mut)]
+    state : Account<'info, SpinItemList>,
+
+    token_mint: Account<'info, Mint>,
+    #[account(
+        init,
+        seeds = [(*rand.key).as_ref()],
+        bump = _token_bump,
+        payer = owner,
+        token::mint = token_mint,
+        token::authority = owner,
+    )]
+    token_vault: Account<'info, TokenAccount>,
+
+    rand : AccountInfo<'info>,
+
+    /// CHECK: this is not dangerous.
+    // #[account(mut)]
+    // reward_account : Account<'info, TokenAccount>,
+
+    /// CHECK: this is not dangerous.
+    #[account(address=spl_token::id())]
+    token_program : AccountInfo<'info>,
+
+    system_program : Program<'info,System>,
+    rent: Sysvar<'info, Rent>
+}
+
+
+#[derive(Accounts)]
+pub struct SpinWheel<'info> {
+    #[account(mut)]
+    state : Account<'info, SpinItemList>,
+}
+
+#[derive(Accounts)]
+pub struct TransferRewards<'info> {
+    /// CHECK: this is not dangerous.
+    #[account(mut, signer)]
+    owner : AccountInfo<'info>, 
+
+    /// CHECK: this is not dangerous.
+    #[account(mut)]
+    state : Account<'info, SpinItemList>,
+
+    token_mint: Account<'info, Mint>,
+    #[account(mut, 
+        constraint = token_vault.mint == token_mint.key(),
+        constraint = token_vault.owner == *owner.key,
+    )]
+    token_vault: Account<'info, TokenAccount>,
+
+    /// CHECK: this is not dangerous.
+    #[account(mut,owner=spl_token::id())]
+    dest_account : AccountInfo<'info>,
+
+    /// CHECK: this is not dangerous.
+    #[account(address=spl_token::id())]
+    token_program : AccountInfo<'info>,
+
+    system_program : Program<'info,System>,
+}
---
+++ tests/spin_win.ts
@@ -0,0 +1,232 @@
+import * as anchor from '@project-serum/anchor';
+import { Program } from '@project-serum/anchor';
+import { SpinWin } from '../target/types/spin_win';
+
+import { PublicKey, SystemProgram, Transaction, Connection, Commitment, SYSVAR_RENT_PUBKEY } from '@solana/web3.js';
+import { TOKEN_PROGRAM_ID, Token } from "@solana/spl-token";
+import { assert } from "chai";
+
+
+describe('spin_win', () => {
+
+  // Configure the client to use the local cluster.
+  const provider = anchor.Provider.env();
+  anchor.setProvider(provider);
+
+  const program = anchor.workspace.SpinWin as Program<SpinWin>;
+
+  let mintA = null as Token;
+  let mintB = null as Token;
+  let initializerTokenAccountA = null;
+  let initializerTokenAccountB = null;
+  let takerTokenAccountA = null;
+  let takerTokenAccountB = null;
+  let vault_account_pda = null;
+  let vault_account_bump = null;
+  let vault_authority_pda = null;
+
+  let pool_account_pda = null;
+  let pool_account_bump = null;
+
+  let token_vault_list = [];
+
+  const takerAmount = 1000;
+  const initializerAmount = 500;
+
+  const escrowAccount = anchor.web3.Keypair.generate();
+  const payer = anchor.web3.Keypair.generate();
+  const mintAuthority = anchor.web3.Keypair.generate();
+  const initializerMainAccount = anchor.web3.Keypair.generate();
+  const takerMainAccount = anchor.web3.Keypair.generate();
+
+  it("Initialize program state", async () => {
+    // Airdropping tokens to a payer.
+    await provider.connection.confirmTransaction(
+      await provider.connection.requestAirdrop(payer.publicKey, 1000000000),
+      "processed"
+    );
+    await provider.connection.confirmTransaction(
+      await provider.connection.requestAirdrop(provider.wallet.publicKey, 1000000000),
+      "processed"
+    );
+
+    // Fund Main Accounts
+    await provider.send(
+      (() => {
+        const tx = new Transaction();
+        tx.add(
+          SystemProgram.transfer({
+            fromPubkey: payer.publicKey,
+            toPubkey: initializerMainAccount.publicKey,
+            lamports: 100000000,
+          }),
+          SystemProgram.transfer({
+            fromPubkey: payer.publicKey,
+            toPubkey: takerMainAccount.publicKey,
+            lamports: 100000000,
+          })
+        );
+        return tx;
+      })(),
+      [payer]
+    );
+
+    mintA = await Token.createMint(
+      provider.connection,
+      payer,
+      mintAuthority.publicKey,
+      null,
+      0,
+      TOKEN_PROGRAM_ID
+    );
+
+    mintB = await Token.createMint(
+      provider.connection,
+      payer,
+      mintAuthority.publicKey,
+      null,
+      0,
+      TOKEN_PROGRAM_ID
+    );
+
+    initializerTokenAccountA = await mintA.createAccount(initializerMainAccount.publicKey);
+    takerTokenAccountA = await mintA.createAccount(takerMainAccount.publicKey);
+
+    initializerTokenAccountB = await mintB.createAccount(initializerMainAccount.publicKey);
+    takerTokenAccountB = await mintB.createAccount(takerMainAccount.publicKey);
+
+    await mintA.mintTo(
+      initializerTokenAccountA,
+      mintAuthority.publicKey,
+      [mintAuthority],
+      initializerAmount
+    );
+
+    await mintB.mintTo(
+      takerTokenAccountB,
+      mintAuthority.publicKey,
+      [mintAuthority],
+      takerAmount
+    );
+
+    let _initializerTokenAccountA = await mintA.getAccountInfo(initializerTokenAccountA);
+    let _takerTokenAccountB = await mintB.getAccountInfo(takerTokenAccountB);
+
+    assert.ok(_initializerTokenAccountA.amount.toNumber() == initializerAmount);
+    assert.ok(_takerTokenAccountB.amount.toNumber() == takerAmount);
+  });
+
+  it("Initialize escrow", async () => {
+    let [_pool, _pool_bump] = await PublicKey.findProgramAddress([Buffer.from(anchor.utils.bytes.utf8.encode("sw_game_seeds"))], program.programId);
+    pool_account_pda = _pool;
+    pool_account_bump = _pool_bump;
+
+    const [_vault_account_pda, _vault_account_bump] = await PublicKey.findProgramAddress(
+      [Buffer.from(anchor.utils.bytes.utf8.encode("sw_token-seed"))],
+      program.programId
+    );
+    vault_account_pda = _vault_account_pda;
+    vault_account_bump = _vault_account_bump;
+
+    const [_vault_authority_pda, _vault_authority_bump] = await PublicKey.findProgramAddress(
+      [Buffer.from(anchor.utils.bytes.utf8.encode("escrow"))],
+      program.programId
+    );
+    vault_authority_pda = _vault_authority_pda;
+
+    console.log('initialize start...');
+
+    await program.rpc.initialize(
+      _pool_bump,
+      {
+        accounts: {
+          initializer: initializerMainAccount.publicKey,
+          state: pool_account_pda,
+          systemProgram: anchor.web3.SystemProgram.programId,
+        },
+        signers: [initializerMainAccount]
+      }
+    );
+
+    console.log('initialize end...');
+  });
+
+  it("Set Item", async () => {
+    console.log('Start to Set Item...');
+
+      let randomPubkey = anchor.web3.Keypair.generate().publicKey;
+      let [_token_vault, _token_vault_bump] = await PublicKey.findProgramAddress([Buffer.from(randomPubkey.toBuffer())], program.programId);
+
+      token_vault_list.push({vault: _token_vault, bump: _token_vault_bump});
+
+      let ratio_list = [];
+      let amount_list = [];
+      for (let i = 0; i < 15; i++) {
+        if (i >= 0 && i <= 4) {
+          ratio_list.push(2);
+        } else if (i >= 5 && i <= 9) {
+          ratio_list.push(10);
+        } else {
+          ratio_list.push(8);
+        }
+        amount_list.push(new anchor.BN(2));
+      }
+
+      await program.rpc.setItem(
+        _token_vault_bump,
+        ratio_list,
+        amount_list,
+        {
+          accounts: {
+            owner: initializerMainAccount.publicKey,
+            state: pool_account_pda,
+            tokenMint: mintA.publicKey,
+            tokenVault: _token_vault,
+            rand: randomPubkey,
+            // rewardAccount: initializerTokenAccountA,
+            tokenProgram: TOKEN_PROGRAM_ID,
+            systemProgram: anchor.web3.SystemProgram.programId,
+            rent: SYSVAR_RENT_PUBKEY
+          },
+          signers: [initializerMainAccount]
+        }
+      );
+
+    console.log('End to Set Item...');
+  });
+
+
+  it("spin_wheel", async () => {
+    console.log('Start to spin_wheel...');
+    await program.rpc.spinWheel({
+      accounts: {
+        state: pool_account_pda,
+      }
+    });
+
+    let _state = await program.account.spinItemList.fetch(
+      pool_account_pda
+    );
+
+    let t_vault_account = token_vault_list[0];
+    console.log('spin token vault : ', t_vault_account);
+
+    console.log('last spin index : ', _state.lastSpinindex);
+    // await program.rpc.transferRewards(
+    //   _state.lastSpinindex,
+    //   {
+    //     accounts: {
+    //       owner: initializerMainAccount.publicKey,
+    //       state: pool_account_pda,
+    //       tokenMint: mintA.publicKey,
+    //       tokenVault: t_vault_account.vault,
+    //       destAccount: initializerTokenAccountA,
+    //       systemProgram: anchor.web3.SystemProgram.programId,
+    //       tokenProgram: TOKEN_PROGRAM_ID,
+    //     },
+    //     signers: [initializerMainAccount]
+    //   });
+
+    console.log('End to spin_wheel...');
+  });
+});
